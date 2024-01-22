545233f70547831a3d82c07b10361908638e3474
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-22 19:42:57 +0700

    add json-server-auth

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-import jsonServer from "json-server-auth";
-import dotenv from "dotenv";
-const server = jsonServer.create();
+import jsonServer from "json-server";
+import auth from "json-server-auth";
+
+const app = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-dotenv.config();
-const port = process.env.PORT || 8080;
 
-server.use(middlewares);
-server.use(router);
+const rules = auth.rewriter({
+  // Permission rules
+  users: 600,
+  messages: 640,
+  // Other rules
+  "/posts/:category": "/posts?category=:category",
+});
 
-server.listen(port);
+// /!\ Bind the router db to the app
+app.db = router.db;
+
+// You must apply the auth middleware before the router
+
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(3000);
