bc651e7e4697297ca3b78a638d567c3a22503f7b
hoangnm.ndm <hoangnm.ndm@gmail.com>
2024-01-22 19:58:44 +0700

    disnabale rules

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ import auth from "json-server-auth";
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
 
-const rules = auth.rewriter({
-  // Permission rules
-  users: 600,
-  messages: 640,
-  // Other rules
-  "/posts/:category": "/posts?category=:category",
-});
+// const rules = auth.rewriter({
+//   // Permission rules
+//   users: 600,
+//   messages: 640,
+//   // Other rules
+//   "/posts/:category": "/posts?category=:category",
+// });
 
 // /!\ Bind the router db to the app
 app.db = router.db;
 
 // You must apply the auth middleware before the router
 
-app.use(rules);
+// app.use(rules);
 app.use(auth);
 app.use(router);
 app.listen(3000);
